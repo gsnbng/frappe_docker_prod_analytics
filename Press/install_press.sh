6f7c62ca8b0fc0cde79535508c36ffca8858a0ea
gsnbng <sheshanarayanag@gmail.com>
2026-08-04 14:40:44 +0530

    Add installation script for Press application
    This script sets up the environment for the Press application, including building a Docker image and managing Docker containers.

---
+++ Press/install_press.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+nano apps.json
+export APPS_JSON_BASE64=$(base64 -w 0 apps.json)
+echo -n ${APPS_JSON_BASE64} | base64 -d > apps-test-output.json
+nano apps-test-output.json
+docker build --no-cache --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe --build-arg=FRAPPE_BRANCH=version-15 --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 --tag=ghcr.io/user/repo/press:0.0.1  --progress=plain --no-cache .
+export CUSTOM_IMAGE='ghcr.io/user/repo/press'
+export CUSTOM_TAG='0.0.1'
+export SITE_NAME1='press_site1'
+export SITE_NAME='press_site'
+docker compose -f pwd.yml down
+docker compose -f pwd.yml up -d
+docker exec -it solidworks_interface-backend-1 bash
